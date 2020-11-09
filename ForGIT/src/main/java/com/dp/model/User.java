74cb0c6bab3da8d655d6680fb2ffc3b989491833
deepika-vuppugandla <deepikareddy61525@gmail.com>
2020-11-09 12:07:57 +0530

    first commit

---
+++ ForGIT/src/main/java/com/dp/model/User.java
@@ -0,0 +1,35 @@
+package com.dp.model;
+
+public class User {
+
+String name;
+String password;
+public String getName() {
+	return name;
+}
+public void setName(String name) {
+	this.name = name;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+@Override
+public String toString() {
+	return "User [name=" + name + ", password=" + password + "]";
+}
+public User(String name, String password) {
+	super();
+	this.name = name;
+	this.password = password;
+}
+public User() {
+	super();
+	// TODO Auto-generated constructor stub
+}
+
+	
+	
+}
